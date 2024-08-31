8ecf17de18184a1958d8e4efdf814add1bc70d71
Raveen <raveenJayasanka41@gmail.com>
2024-08-31 12:35:16 +0530

    Add project files.

---
+++ Login_Page.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFModernVerticalMenu
+{
+    /// <summary>
+    /// Interaction logic for Login_Page.xaml
+    /// </summary>
+    public partial class Login_Page : Window
+    {
+        public Login_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m1 = new MainWindow();
+            this.Hide();
+            m1.Show();
+        }
+
+        private void btnexit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void themetoggle_Click(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
---
+++ loadingPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFModernVerticalMenu
+{
+    /// <summary>
+    /// Interaction logic for loadingPage.xaml
+    /// </summary>
+    public partial class loadingPage : Window
+    {
+        public loadingPage()
+        {
+            InitializeComponent();
+        }
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker bk = new BackgroundWorker();
+            bk.WorkerReportsProgress = true;
+            bk.DoWork += bk_DoWork;
+            bk.ProgressChanged += bk_ProgressChanged;
+            bk.RunWorkerAsync();
+        }
+        void bk_DoWork(object sender, EventArgs e)
+        {
+            //A Thread for Run the Loading
+            for (int i = 0; i <= 80; i++)
+            {
+
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(80);
+            }
+        }
+
+        void bk_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            ProgressBar.Value = e.ProgressPercentage;
+            if (ProgressBar.Value == 80)
+            {
+                Login_Page lP = new Login_Page();
+                lP.Show();
+                this.Close();
+            }
+
+        }
+    }
+}
+
