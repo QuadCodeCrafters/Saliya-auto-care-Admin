747cbbcf0b9449915c2960b5f3f3ee8c667dfe15
Raveen <raveenJayasanka41@gmail.com>
2024-09-05 05:45:38 +0530

    Updated the backend of the Login UI (in MYSQL workbench,Download the nuget package for MYSQL)

--- Login_Page.xaml.cs
+++ Login_Page.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Windows.Media.Animation;
 
 namespace WPFModernVerticalMenu
 {
@@ -20,17 +23,60 @@ namespace WPFModernVerticalMenu
     /// </summary>
     public partial class Login_Page : Window
     {
-        public Login_Page()
+        DataBaseConnection d1 = null;
+        public Login_Page(DataBaseConnection obj)
         {
             InitializeComponent();
+            d1 = obj;
         }
 
         private void btnlogin_Click(object sender, RoutedEventArgs e)
         {
-            //add password and username for database 
-            MainWindow m1 = new MainWindow();
-            this.Hide();
-            m1.Show();
+            if (txtusername.Text =="" || txtpassword.Password == "")
+            {
+                if(txtusername.Text == "")
+                {
+                    accError.Text = "Please fill the user name";
+                }
+                if(txtpassword.Password == "")
+                {
+                    accError.Text = "Please fill the password";
+                }
+                if(txtusername.Text == "" && txtpassword.Password == "")
+                {
+                    accError.Text = "Please fill user name and the password";
+                }
+               
+            }
+            else
+            {
+                //add password and username for database
+                string usName = txtusername.Text,pass = txtpassword.Password;
+                 d1.con.Open();
+                string query = "SELECT UserName,password FROM useraccounts WHERE UserName = @v1 AND password = @v2";
+                MySqlCommand cmd = new MySqlCommand(query, d1.con);
+                cmd.Parameters.AddWithValue("@v1",usName);
+                cmd.Parameters.AddWithValue("@v2",pass);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                string us = "";
+                string pas = "";
+                while (reader.Read())
+                {
+                     us = reader.GetString("UserName");
+                     pas = reader.GetString("password");
+                }
+                if(us != "" && pas != "")
+                {
+                     MainWindow m1 = new MainWindow();
+                     this.Hide();
+                     m1.Show();
+                }
+                else
+                {
+                    accError.Text = "Incorrect user name or password";
+                }
+                d1.con.Close();
+            }
         }
 
         private void btnexit_Click(object sender, RoutedEventArgs e)
